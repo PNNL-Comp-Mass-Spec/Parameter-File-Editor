35404ac6ae02adc1c1f66a15b3db3fadd72adb7b
alchemistmatt <matthew.monroe@pnnl.gov>
2021-09-24 09:55:31 -0700

    Suppress warningss

---
+++ ParamFileGenerator/GlobalSuppressions.vb
@@ -0,0 +1,35 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.c_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.d_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.v_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.w_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.x_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.y_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamFileGenerator.clsIonSeries.z_Ion_Weighting")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsDBTask")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsDMSParamStorage")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsDynamicMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsEnzymeCollection")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsEnzymeDetails")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsGetEnzymeBlock")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsGetResiduesList")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsIonSeries")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsIsoMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsMainProcess")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsModEntry")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsParams")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsReconstituteIsoMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsRetrieveParams")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsStaticMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsTermDynamicMods")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.clsWriteOutput")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.DownloadParams.clsParamsFromDMS")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.DownloadParams.clsParamsFromDMS.eParamFileTypeConstants")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamFileGenerator.MakeParams.clsMakeParameterFile")>
---
+++ ParamGenTest/GlobalSuppressions.vb
@@ -0,0 +1,29 @@
+﻿' This file is used by Code Analysis to maintain SuppressMessage
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given
+' a specific target and scoped to a namespace, type, member, etc.
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.cboAvailableParams_SelectedIndexChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.cboFileTypes_SelectedIndexChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.cmdDoIt_Click(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.txtDMSConnectionString_TextChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.txtFASTAPath_TextChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~M:ParamGenTest.frmMain.txtOutputPath_TextChanged(System.Object,System.EventArgs)")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.cboAvailableParams")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.cboFileTypes")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.cmdDoIt")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblConnectionString")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblDatasetID")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblOutputPath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblParamFileName")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblParamFileType")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.lblPickList")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtDatasetID")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtDMSConnectionString")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtFASTAPath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtOutputPath")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtParamFileName")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="member", Target:="~P:ParamGenTest.frmMain.txtResults")>
+<Assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification:="Allowed name", Scope:="type", Target:="~T:ParamGenTest.frmMain")>
