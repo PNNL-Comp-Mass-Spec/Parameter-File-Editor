02019c8e6ba61005a207af8d0ba76b468101e7d3
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-14 10:42:08 -0800

    Resize textboxes when resizing the window

--- ParamGenTest/Main.Designer.cs
+++ ParamGenTest/Main.Designer.cs
@@ -32,185 +32,203 @@ namespace ParamGenTest
         /// </summary>
         private void InitializeComponent()
         {
-            txtOutputPath = new TextBox();
-            txtDMSConnectionString = new TextBox();
-            cboFileTypes = new ComboBox();
-            lblOutputPath = new Label();
-            lblPickList = new Label();
-            lblConnectionString = new Label();
-            lblParamFileType = new Label();
-            cboAvailableParams = new ComboBox();
-            txtResults = new TextBox();
-            cmdDoIt = new Button();
-            txtFASTAPath = new TextBox();
-            txtDatasetID = new TextBox();
-            lblDatasetID = new Label();
-            txtParamFileName = new TextBox();
-            lblParamFileName = new Label();
-            lblFASTAFilePath = new Label();
-            SuspendLayout();
-            //
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.txtDMSConnectionString = new System.Windows.Forms.TextBox();
+            this.cboFileTypes = new System.Windows.Forms.ComboBox();
+            this.lblOutputPath = new System.Windows.Forms.Label();
+            this.lblPickList = new System.Windows.Forms.Label();
+            this.lblConnectionString = new System.Windows.Forms.Label();
+            this.lblParamFileType = new System.Windows.Forms.Label();
+            this.cboAvailableParams = new System.Windows.Forms.ComboBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.cmdDoIt = new System.Windows.Forms.Button();
+            this.txtFASTAPath = new System.Windows.Forms.TextBox();
+            this.txtDatasetID = new System.Windows.Forms.TextBox();
+            this.lblDatasetID = new System.Windows.Forms.Label();
+            this.txtParamFileName = new System.Windows.Forms.TextBox();
+            this.lblParamFileName = new System.Windows.Forms.Label();
+            this.lblFASTAFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
             // txtOutputPath
-            //
-            txtOutputPath.Location = new Point(156, 20);
-            txtOutputPath.Name = "txtOutputPath";
-            txtOutputPath.Size = new Size(400, 20);
-            txtOutputPath.TabIndex = 0;
-            txtOutputPath.Text = @"F:\Temp\";
-            txtOutputPath.TextChanged += new EventHandler(txtOutputPath_TextChanged);
-            //
+            // 
+            this.txtOutputPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputPath.Location = new System.Drawing.Point(156, 20);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.Size = new System.Drawing.Size(400, 20);
+            this.txtOutputPath.TabIndex = 0;
+            this.txtOutputPath.Text = "F:\\Temp\\";
+            this.txtOutputPath.TextChanged += new System.EventHandler(this.txtOutputPath_TextChanged);
+            // 
             // txtDMSConnectionString
-            //
-            txtDMSConnectionString.Location = new Point(156, 125);
-            txtDMSConnectionString.Name = "txtDMSConnectionString";
-            txtDMSConnectionString.Size = new Size(400, 20);
-            txtDMSConnectionString.TabIndex = 2;
-            txtDMSConnectionString.Text = "Data Source=gigasax;Initial Catalog=DMS5;Integrated Security=SSPI;";
-            txtDMSConnectionString.TextChanged += new EventHandler(txtDMSConnectionString_TextChanged);
-            //
+            // 
+            this.txtDMSConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDMSConnectionString.Location = new System.Drawing.Point(156, 125);
+            this.txtDMSConnectionString.Name = "txtDMSConnectionString";
+            this.txtDMSConnectionString.Size = new System.Drawing.Size(400, 20);
+            this.txtDMSConnectionString.TabIndex = 2;
+            this.txtDMSConnectionString.Text = "Data Source=gigasax;Initial Catalog=DMS5;Integrated Security=SSPI;";
+            this.txtDMSConnectionString.TextChanged += new System.EventHandler(this.txtDMSConnectionString_TextChanged);
+            // 
             // cboFileTypes
-            //
-            cboFileTypes.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboFileTypes.Location = new Point(156, 165);
-            cboFileTypes.Name = "cboFileTypes";
-            cboFileTypes.Size = new Size(400, 21);
-            cboFileTypes.TabIndex = 3;
-            cboFileTypes.SelectedIndexChanged += new EventHandler(cboFileTypes_SelectedIndexChanged);
-            //
+            // 
+            this.cboFileTypes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboFileTypes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFileTypes.Location = new System.Drawing.Point(156, 165);
+            this.cboFileTypes.Name = "cboFileTypes";
+            this.cboFileTypes.Size = new System.Drawing.Size(400, 21);
+            this.cboFileTypes.TabIndex = 3;
+            this.cboFileTypes.SelectedIndexChanged += new System.EventHandler(this.cboFileTypes_SelectedIndexChanged);
+            // 
             // lblOutputPath
-            //
-            lblOutputPath.Location = new Point(16, 24);
-            lblOutputPath.Name = "lblOutputPath";
-            lblOutputPath.Size = new Size(120, 23);
-            lblOutputPath.TabIndex = 4;
-            lblOutputPath.Text = "Output Path";
-            //
+            // 
+            this.lblOutputPath.Location = new System.Drawing.Point(16, 24);
+            this.lblOutputPath.Name = "lblOutputPath";
+            this.lblOutputPath.Size = new System.Drawing.Size(120, 23);
+            this.lblOutputPath.TabIndex = 4;
+            this.lblOutputPath.Text = "Output Path";
+            // 
             // lblPickList
-            //
-            lblPickList.Location = new Point(16, 64);
-            lblPickList.Name = "lblPickList";
-            lblPickList.Size = new Size(120, 17);
-            lblPickList.TabIndex = 5;
-            lblPickList.Text = "Param File Pick List";
-            //
+            // 
+            this.lblPickList.Location = new System.Drawing.Point(16, 64);
+            this.lblPickList.Name = "lblPickList";
+            this.lblPickList.Size = new System.Drawing.Size(120, 17);
+            this.lblPickList.TabIndex = 5;
+            this.lblPickList.Text = "Param File Pick List";
+            // 
             // lblConnectionString
-            //
-            lblConnectionString.Location = new Point(16, 125);
-            lblConnectionString.Name = "lblConnectionString";
-            lblConnectionString.Size = new Size(120, 23);
-            lblConnectionString.TabIndex = 6;
-            lblConnectionString.Text = "Connect String";
-            //
+            // 
+            this.lblConnectionString.Location = new System.Drawing.Point(16, 125);
+            this.lblConnectionString.Name = "lblConnectionString";
+            this.lblConnectionString.Size = new System.Drawing.Size(120, 23);
+            this.lblConnectionString.TabIndex = 6;
+            this.lblConnectionString.Text = "Connect String";
+            // 
             // lblParamFileType
-            //
-            lblParamFileType.Location = new Point(16, 169);
-            lblParamFileType.Name = "lblParamFileType";
-            lblParamFileType.Size = new Size(120, 23);
-            lblParamFileType.TabIndex = 7;
-            lblParamFileType.Text = "Param File Type";
-            //
+            // 
+            this.lblParamFileType.Location = new System.Drawing.Point(16, 169);
+            this.lblParamFileType.Name = "lblParamFileType";
+            this.lblParamFileType.Size = new System.Drawing.Size(120, 23);
+            this.lblParamFileType.TabIndex = 7;
+            this.lblParamFileType.Text = "Param File Type";
+            // 
             // cboAvailableParams
-            //
-            cboAvailableParams.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboAvailableParams.Location = new Point(156, 60);
-            cboAvailableParams.Name = "cboAvailableParams";
-            cboAvailableParams.Size = new Size(400, 21);
-            cboAvailableParams.Sorted = true;
-            cboAvailableParams.TabIndex = 8;
-            cboAvailableParams.SelectedIndexChanged += new EventHandler(cboAvailableParams_SelectedIndexChanged);
-            //
+            // 
+            this.cboAvailableParams.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboAvailableParams.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboAvailableParams.Location = new System.Drawing.Point(156, 60);
+            this.cboAvailableParams.Name = "cboAvailableParams";
+            this.cboAvailableParams.Size = new System.Drawing.Size(400, 21);
+            this.cboAvailableParams.Sorted = true;
+            this.cboAvailableParams.TabIndex = 8;
+            this.cboAvailableParams.SelectedIndexChanged += new System.EventHandler(this.cboAvailableParams_SelectedIndexChanged);
+            // 
             // txtResults
-            //
-            txtResults.Location = new Point(24, 253);
-            txtResults.Multiline = true;
-            txtResults.Name = "txtResults";
-            txtResults.Size = new Size(384, 84);
-            txtResults.TabIndex = 9;
-            //
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Location = new System.Drawing.Point(24, 253);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(384, 84);
+            this.txtResults.TabIndex = 9;
+            // 
             // cmdDoIt
-            //
-            cmdDoIt.Location = new Point(436, 305);
-            cmdDoIt.Name = "cmdDoIt";
-            cmdDoIt.Size = new Size(124, 32);
-            cmdDoIt.TabIndex = 10;
-            cmdDoIt.Text = "&Go";
-            cmdDoIt.Click += new EventHandler(cmdDoIt_Click);
-            //
+            // 
+            this.cmdDoIt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdDoIt.Location = new System.Drawing.Point(436, 305);
+            this.cmdDoIt.Name = "cmdDoIt";
+            this.cmdDoIt.Size = new System.Drawing.Size(124, 32);
+            this.cmdDoIt.TabIndex = 10;
+            this.cmdDoIt.Text = "&Go";
+            this.cmdDoIt.Click += new System.EventHandler(this.cmdDoIt_Click);
+            // 
             // txtFASTAPath
-            //
-            txtFASTAPath.Location = new Point(160, 209);
-            txtFASTAPath.Name = "txtFASTAPath";
-            txtFASTAPath.Size = new Size(396, 20);
-            txtFASTAPath.TabIndex = 11;
-            txtFASTAPath.Text = @"C:\DMS_Temp_Org\bsa.fasta";
-            txtFASTAPath.TextChanged += new EventHandler(txtFASTAPath_TextChanged);
-            //
+            // 
+            this.txtFASTAPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFASTAPath.Location = new System.Drawing.Point(160, 209);
+            this.txtFASTAPath.Name = "txtFASTAPath";
+            this.txtFASTAPath.Size = new System.Drawing.Size(396, 20);
+            this.txtFASTAPath.TabIndex = 11;
+            this.txtFASTAPath.Text = "C:\\DMS_Temp_Org\\bsa.fasta";
+            this.txtFASTAPath.TextChanged += new System.EventHandler(this.txtFASTAPath_TextChanged);
+            // 
             // txtDatasetID
-            //
-            txtDatasetID.Location = new Point(436, 279);
-            txtDatasetID.Name = "txtDatasetID";
-            txtDatasetID.Size = new Size(120, 20);
-            txtDatasetID.TabIndex = 12;
-            txtDatasetID.Text = "101986";
-            txtDatasetID.TextAlign = HorizontalAlignment.Right;
-            //
+            // 
+            this.txtDatasetID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDatasetID.Location = new System.Drawing.Point(436, 279);
+            this.txtDatasetID.Name = "txtDatasetID";
+            this.txtDatasetID.Size = new System.Drawing.Size(120, 20);
+            this.txtDatasetID.TabIndex = 12;
+            this.txtDatasetID.Text = "101986";
+            this.txtDatasetID.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // lblDatasetID
-            //
-            lblDatasetID.AutoSize = true;
-            lblDatasetID.Location = new Point(436, 260);
-            lblDatasetID.Name = "lblDatasetID";
-            lblDatasetID.Size = new Size(121, 15);
-            lblDatasetID.TabIndex = 13;
-            lblDatasetID.Text = "(Optional) Dataset ID";
-            //
+            // 
+            this.lblDatasetID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDatasetID.AutoSize = true;
+            this.lblDatasetID.Location = new System.Drawing.Point(436, 260);
+            this.lblDatasetID.Name = "lblDatasetID";
+            this.lblDatasetID.Size = new System.Drawing.Size(106, 13);
+            this.lblDatasetID.TabIndex = 13;
+            this.lblDatasetID.Text = "(Optional) Dataset ID";
+            // 
             // txtParamFileName
-            //
-            txtParamFileName.Location = new Point(156, 87);
-            txtParamFileName.Name = "txtParamFileName";
-            txtParamFileName.Size = new Size(400, 20);
-            txtParamFileName.TabIndex = 14;
-            //
+            // 
+            this.txtParamFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtParamFileName.Location = new System.Drawing.Point(156, 87);
+            this.txtParamFileName.Name = "txtParamFileName";
+            this.txtParamFileName.Size = new System.Drawing.Size(400, 20);
+            this.txtParamFileName.TabIndex = 14;
+            // 
             // lblParamFileName
-            //
-            lblParamFileName.Location = new Point(16, 87);
-            lblParamFileName.Name = "lblParamFileName";
-            lblParamFileName.Size = new Size(120, 20);
-            lblParamFileName.TabIndex = 15;
-            lblParamFileName.Text = "Param File Name";
-            //
+            // 
+            this.lblParamFileName.Location = new System.Drawing.Point(16, 87);
+            this.lblParamFileName.Name = "lblParamFileName";
+            this.lblParamFileName.Size = new System.Drawing.Size(120, 20);
+            this.lblParamFileName.TabIndex = 15;
+            this.lblParamFileName.Text = "Param File Name";
+            // 
             // lblFASTAFilePath
-            //
-            lblFASTAFilePath.Location = new Point(16, 203);
-            lblFASTAFilePath.Name = "lblFASTAFilePath";
-            lblFASTAFilePath.Size = new Size(120, 41);
-            lblFASTAFilePath.TabIndex = 16;
-            lblFASTAFilePath.Text = "FASTA File Path (only for SEQUEST)";
-            //
+            // 
+            this.lblFASTAFilePath.Location = new System.Drawing.Point(16, 203);
+            this.lblFASTAFilePath.Name = "lblFASTAFilePath";
+            this.lblFASTAFilePath.Size = new System.Drawing.Size(120, 41);
+            this.lblFASTAFilePath.TabIndex = 16;
+            this.lblFASTAFilePath.Text = "FASTA File Path (only for SEQUEST)";
+            // 
             // Main
-            //
-            AutoScaleBaseSize = new Size(5, 13);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 346);
-            Controls.Add(lblFASTAFilePath);
-            Controls.Add(lblParamFileName);
-            Controls.Add(txtParamFileName);
-            Controls.Add(lblDatasetID);
-            Controls.Add(txtDatasetID);
-            Controls.Add(txtFASTAPath);
-            Controls.Add(txtResults);
-            Controls.Add(txtDMSConnectionString);
-            Controls.Add(txtOutputPath);
-            Controls.Add(cmdDoIt);
-            Controls.Add(cboAvailableParams);
-            Controls.Add(lblParamFileType);
-            Controls.Add(lblConnectionString);
-            Controls.Add(lblPickList);
-            Controls.Add(lblOutputPath);
-            Controls.Add(cboFileTypes);
+            this.Controls.Add(this.lblFASTAFilePath);
+            this.Controls.Add(this.lblParamFileName);
+            this.Controls.Add(this.txtParamFileName);
+            this.Controls.Add(this.lblDatasetID);
+            this.Controls.Add(this.txtDatasetID);
+            this.Controls.Add(this.txtFASTAPath);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtDMSConnectionString);
+            this.Controls.Add(this.txtOutputPath);
+            this.Controls.Add(this.cmdDoIt);
+            this.Controls.Add(this.cboAvailableParams);
+            this.Controls.Add(this.lblParamFileType);
+            this.Controls.Add(this.lblConnectionString);
+            this.Controls.Add(this.lblPickList);
+            this.Controls.Add(this.lblOutputPath);
+            this.Controls.Add(this.cboFileTypes);
             this.Name = "Main";
-            Text = "DMS Param File Generator";
-            ResumeLayout(false);
-            PerformLayout();
+            this.Text = "DMS Param File Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
