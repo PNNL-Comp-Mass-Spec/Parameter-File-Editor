0fab1157b3521ce51c07c1eb0bb2c5f07c661411
alchemistmatt <matthew.monroe@pnnl.gov>
2024-10-01 20:14:02 -0700

    Update batch file

--- PushFiles_to_PAST_Software_and_AnalysisManager.bat
+++ PushFiles_to_PAST_Software_and_AnalysisManager.bat
@@ -1,10 +1,13 @@
+xcopy ParamFileEditor\bin\Debug\ParamFileEditor.exe \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
+xcopy ParamFileEditor\bin\Debug\ParamFileEditor.xml \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
+xcopy ParamFileEditor\bin\Debug\*.dll               \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
+xcopy ParamFileEditor\bin\Debug\ParamFileEditorSettings.xml \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
+
+xcopy ParamGenTest\bin\Debug\ParamGenTest.exe       \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
+xcopy ParamGenTest\bin\Debug\ParamGenTest.xml       \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
 xcopy ParamGenTest\bin\Debug\ParamFileGenerator.dll \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
 xcopy ParamGenTest\bin\Debug\ParamFileGenerator.xml \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
-xcopy ParamGenTest\bin\Debug\PRISM.dll \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
-xcopy ParamGenTest\bin\Debug\ParamGenTest.exe \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
-xcopy ParamGenTest\bin\Debug\ParamGenTest.xml \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
-xcopy ParamGenTest\bin\Debug\*.dll \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
-
+xcopy ParamGenTest\bin\Debug\*.dll                  \\proto-2\PAST\Software\Sequest_Param_File_Editor\ /Y /D
 
 xcopy ParamFileGenerator\bin\Debug\*.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common /Y /D
 
